5fe44d342d120fc1bf1621afb9df24285413c1c6
JMDeS <jmichaeldeseno@gmail.com>
2017-04-21 12:13:21 -0400

    "need to change mocks"

--- app/tests/auto/tests/pump.cpp
+++ app/tests/auto/tests/pump.cpp
@@ -10,7 +10,7 @@ Pump::Pump()
 
 int Pump::connectPump()
 {
-//    system("\"C:\\Program Files\\PuTTY\\putty.exe\" -serial com4");
+    system("\"C:\\Program Files\\PuTTY\\putty.exe\" -serial com4");
 
     return 0;
 }
@@ -18,15 +18,18 @@ int Pump::connectPump()
 
 int Pump::startPump()
 {
+    system("\"C:\\Program Files\\PuTTY\\putty.exe\" -serial com4 \"start\"");
     return 0;
 }
 
 int Pump::stopPump()
 {
+    system("\"C:\\Program Files\\PuTTY\\putty.exe\" -serial com4 \"stop\"");
     return 0;
 }
 
 int Pump::setRate(double flowrate)
 {
+    system("\"C:\\Program Files\\PuTTY\\putty.exe\" -serial com4 \"set rate "+flowrate+"\"");
     return 0;
 }
