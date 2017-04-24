3bc498ecfde9893eeed327922b89fb35c976bdbc
JMDeS <jmichaeldeseno@gmail.com>
2017-04-23 21:07:54 -0400

    removed unused code TODO: convert Camera to widget and attach it to MainWindow UI

--- app/tests/auto/tests/camera.h
+++ app/tests/auto/tests/camera.h
@@ -62,46 +62,9 @@ public:
 private slots:
     void setCamera(const QCameraInfo &cameraInfo);
 
-    void startCamera();
-    void stopCamera();
-
-    void record();
-    void pause();
-    void stop();
-    void setMuted(bool);
-
-    void toggleLock();
-    void takeImage();
-    void displayCaptureError(int, QCameraImageCapture::Error, const QString &errorString);
-
-
-
-    void displayRecorderError();
-    void displayCameraError();
-
     void updateCameraDevice(QAction *action);
 
-    void updateCameraState(QCamera::State);
-    void updateCaptureMode();
-    void updateRecorderState(QMediaRecorder::State state);
-    void setExposureCompensation(int index);
-
-    void updateRecordTime();
-
-    void processCapturedImage(int requestId, const QImage &img);
-    void updateLockStatus(QCamera::LockStatus, QCamera::LockChangeReason);
-
     void displayViewfinder();
-    void displayCapturedImage();
-
-    void readyForCapture(bool ready);
-    void imageSaved(int id, const QString &fileName);
-
-
-protected:
-    void keyPressEvent(QKeyEvent *event);
-    void keyReleaseEvent(QKeyEvent *event);
-    void closeEvent(QCloseEvent *event);
 
 private:
     Ui::Camera *ui;
@@ -109,13 +72,6 @@ private:
     QCamera *camera;
     QCameraImageCapture *imageCapture;
     QMediaRecorder* mediaRecorder;
-
-    QImageEncoderSettings imageSettings;
-    QAudioEncoderSettings audioSettings;
-    QVideoEncoderSettings videoSettings;
-    QString videoContainerFormat;
-    bool isCapturingImage;
-    bool applicationExiting;
 };
 
 #endif
